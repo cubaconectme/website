75f02bbd415890ad8da355890e0c0bab6f0b9f8b
Henry <henrycarmenateg@gmail.com>
2019-03-19 19:53:54 -0400

    working around4

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -127,9 +127,17 @@
     </div>
     <div id="fb-root"></div>
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.2&appId=1503393679796720&autoLogAppEvents=1"></script>
-    <script>
-
-    </script>
-   
+    <!--Vendor-JS-->
+    <script src="{{ asset('front/js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <script src="{{ asset('front/js/vendor/bootstrap.min.js') }}"></script>
+    <!--Plugin-JS-->
+    <script src="{{ asset('front/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('front/js/contact-form.js') }}"></script>
+    <script src="{{ asset('front/js/jquery.parallax-1.1.3.js') }}"></script>
+    <script src="{{ asset('front/js/scrollUp.min.js') }}"></script>
+    <script src="{{ asset('front/js/magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('front/js/wow.min.js') }}"></script>
+    <!--Main-active-JS-->
+    <script src="{{ asset('front/js/main.js') }}"></script>
 </body>
 </html>

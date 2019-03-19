d6bfa31197e9b843408583d422ab52855f8f8e1c
Henry <henrycarmenateg@gmail.com>
2019-03-19 19:50:56 -0400

    working around3

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -130,17 +130,6 @@
     <script>
 
     </script>
-    <!--Vendor-JS-->
-    <script src="{{ asset('front/js/vendor/jquery-1.12.4.min.js') }}"></script>
-    <script src="{{ asset('front/js/vendor/bootstrap.min.js') }}"></script>
-    <!--Plugin-JS-->
-    <script src="{{ asset('front/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('front/js/contact-form.js') }}"></script>
-    <script src="{{ asset('front/js/jquery.parallax-1.1.3.js') }}"></script>
-    <script src="{{ asset('front/js/scrollUp.min.js') }}"></script>
-    <script src="{{ asset('front/js/magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('front/js/wow.min.js') }}"></script>
-    <!--Main-active-JS-->
-    <script src="{{ asset('front/js/main.js') }}"></script>
+   
 </body>
 </html>

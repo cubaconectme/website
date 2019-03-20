c0bb7eb1ed676f9b24e007c4469c53313a1b7dca
Henry <henrycarmenateg@gmail.com>
2019-03-20 10:47:49 -0400

    testing forge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,6 +37,7 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function indexView(){
+        dd('test');
         $products = Product::with(['planes' => function($plan){
             $plan->with('promotions');
         }])->get();

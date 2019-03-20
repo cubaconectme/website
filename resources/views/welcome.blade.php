e31a49ffd601ff87c356b426946be4bba93e2355
Henry <henrycarmenateg@gmail.com>
2019-03-20 10:58:46 -0400

    Testing new things

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.front')
 @section('content')
     <front_dashboard :products="{{ json_encode($products) }}" :user="{{ json_encode($user) }}"></front_dashboard>
-    {!! dd('test3' ) !!}
 @endsection
